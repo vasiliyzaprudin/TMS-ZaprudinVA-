18a45fd408b9635e4192ff57142b4e87d1cc71bc
vasiliyzaprudin <vasiliyzaprudin@gmail.com>
2026-03-16 08:40:21 +0500

    В ветку branch добавлен System.out.println.

--- src/main/java/lesson3/lesson3.java
+++ src/main/java/lesson3/lesson3.java
@@ -3,5 +3,6 @@ package lesson3;
 public class lesson3 {
     public static void main(String[] args) {
         System.out.println("Это домашняя работа по теме Git.");
+        System.out.println("Создана новая ветка.");
     }
 }
